20b211069b300be921127e846754f7c533da767d
Faisal Abuzaid <faisal.a@jasaratech.com>
2021-12-29 22:47:41 +0200

    fix app

--- startup/prod.js
+++ startup/prod.js
@@ -1,6 +1,7 @@
+const express = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
-
+const app = express();
 module.exports = function () {
   app.use(helmet());
   app.use(compression());
